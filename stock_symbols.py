b3203b2d5cfe35501c9fd7109f4b9f41dda985ff
thammo4 <thammo4@uic.edu>
2023-11-14 20:22:18 -0500

    Create file with common large-cap stock ticker symbols for convenience

---
+++ stock_symbols.py
@@ -0,0 +1,77 @@
+blue_chip_stocks = [
+	'AA', 'ABBV', 'AXP', 'BA', 'BAC', 'C',
+	'CAT', 'CI', 'CVX', 'DD', 'DIS', 'GE',
+	'GM', 'HD', 'HPQ', 'IBM', 'JNJ', 'JPM',
+	'KO', 'MCD', 'MMM', 'MRK', 'PFE', 'PG',
+	'T', 'VZ', 'WMT', 'XOM'
+];
+
+SP100 = [ 
+	'AAPL', 'ABBV', 'ABT', 'ACN', 'ADBE', 'AIG', 'AMD', 'AMGN', 'AMT', 'AMZN', 'AVGO',
+    'AXP', 'BA', 'BAC', 'BK', 'BKNG', 'BLK', 'BMY', 'BRK.B', 'C', 'CAT', 'CHTR', 'CL',
+    'CMCSA', 'COF', 'COP', 'COST', 'CRM', 'CSCO', 'CVS', 'CVX', 'DE', 'DHR', 'DIS', 'DOW',
+    'DUK', 'EMR', 'EXC', 'F', 'FDX', 'GD', 'GE', 'GILD', 'GM', 'GOOG', 'GOOGL', 'GS', 'HD',
+    'HON', 'IBM', 'INTC', 'JNJ', 'JPM', 'KHC', 'KO', 'LIN', 'LLY', 'LMT', 'LOW', 'MA', 'MCD',
+    'MDLZ', 'MDT', 'MET', 'META', 'MMM', 'MO', 'MRK', 'MS', 'MSFT', 'NEE', 'NFLX', 'NKE',
+    'NVDA', 'ORCL', 'PEP', 'PFE', 'PG', 'PM', 'PYPL', 'QCOM', 'RTX', 'SBUX', 'SCHW', 'SO',
+    'SPG', 'T', 'TGT', 'TMO', 'TMUS', 'TSLA', 'TXN', 'UNH', 'UNP', 'UPS', 'USB', 'V', 'VZ',
+    'WFC', 'WMT', 'XOM'
+];
+
+
+DJIndustry = [
+    'MMM',   # 3M
+    'AXP',   # American Express
+    'AMGN',  # Amgen
+    'AAPL',  # Apple
+    'BA',    # Boeing
+    'CAT',   # Caterpillar
+    'CVX',   # Chevron
+    'CSCO',  # Cisco
+    'KO',    # Coca-Cola
+    'DOW',   # Dow
+    'GS',    # Goldman Sachs
+    'HD',    # Home Depot
+    'HON',   # Honeywell
+    'IBM',   # IBM
+    'INTC',  # Intel
+    'JNJ',   # Johnson & Johnson
+    'JPM',   # JPMorgan Chase
+    'MCD',   # McDonald's
+    'MRK',   # Merck
+    'MSFT',  # Microsoft
+    'NKE',   # Nike
+    'PG',    # Procter & Gamble
+    'CRM',   # Salesforce
+    'TRV',   # Travelers
+    'UNH',   # UnitedHealth
+    'VZ',    # Verizon
+    'V',     # Visa
+    'WBA',   # Walgreens Boots Alliance
+    'WMT',   # Walmart
+    'DIS'    # Walt Disney
+];
+
+DJTransport = [
+    'ALK',   # Alaska Air
+    'AAL',   # American Airlines
+    'CAR',   # Avis Budget Group
+    'CHRW',  # C. H. Robinson
+    'CSX',   # CSX
+    'DAL',   # Delta Air Lines
+    'EXPD',  # Expeditors
+    'FDX',   # FedEx
+    'JBHT',  # J.B. Hunt
+    'JBLU',  # JetBlue
+    'KEX',   # Kirby
+    'LSTR',  # Landstar System
+    'MATX',  # Matson
+    'NSC',   # Norfolk Southern
+    'ODFL',  # Old Dominion Freight Line
+    'R',     # Ryder
+    'LUV',   # Southwest Airlines
+    'UNP',   # Union Pacific
+    'UAL',   # United Airlines
+    'UPS'    # United Parcel Service
+];
+
